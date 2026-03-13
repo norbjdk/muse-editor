8166ce27666ced0c26d9e19641c64f99c1d5e8e9
ndominkiewicz <norbertd1410@gmail.com>
2026-03-14 00:25:14 +0100

    feat: add NavigationBar with simple, user-friendly UI
    - Add many buttons that are going to be used for view switching by ViewManager
    - No events linked yet
    - Using ikonli icons with addIcon from ButtonFactory
    - Add components.css with a little styling

---
+++ src/main/java/com/norbjdk/museeditor/ui/component/NavigationBar.java
@@ -0,0 +1,81 @@
+package com.norbjdk.museeditor.ui.component;
+
+import com.norbjdk.museeditor.ui.model.Presentable;
+import com.norbjdk.museeditor.ui.util.ButtonFactory;
+import com.norbjdk.museeditor.ui.util.SpaceFactory;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
+
+import java.util.Objects;
+
+import static com.norbjdk.museeditor.ui.util.SpaceFactory.createSpacer;
+
+public class NavigationBar extends HBox implements Presentable {
+
+    private Button homeBtn;
+    private Button createProjectBtn;
+    private Button openProjectBtn;
+    private Button collectionBtn;
+    private Button learnBtn;
+    private Button settingsBtn;
+    private Button currentProjectBtn;
+
+    public NavigationBar() {
+        present();
+    }
+
+    @Override
+    public void initComponents() {
+        homeBtn = ButtonFactory.createButton("Home", "home-btn", "Home View", "navigation-btn");
+        createProjectBtn = ButtonFactory.createButton("New Project", "home-btn", "Home View", "navigation-btn");
+        openProjectBtn = ButtonFactory.createButton("Open", "home-btn", "Home View", "navigation-btn");
+        collectionBtn = ButtonFactory.createButton("Collection", "home-btn", "Home View", "navigation-btn");
+        learnBtn = ButtonFactory.createButton("Learn", "home-btn", "Home View", "navigation-btn");
+        settingsBtn = ButtonFactory.createButton("Settings", "home-btn", "Home View", "navigation-btn");
+        currentProjectBtn = ButtonFactory.createButton("Project Name", "project-btn", "Go to your open project", "navigation-btn");
+    }
+
+    @Override
+    public void setupComponents() {
+        ButtonFactory.addIcon(homeBtn, FontAwesomeSolid.HOME, 15, Color.rgb(5, 5, 5));
+        ButtonFactory.addIcon(createProjectBtn, FontAwesomeSolid.PLUS_CIRCLE, 15, Color.rgb(5, 5, 5));
+        ButtonFactory.addIcon(openProjectBtn, FontAwesomeSolid.FOLDER_OPEN, 15, Color.rgb(5, 5, 5));
+        ButtonFactory.addIcon(currentProjectBtn, FontAwesomeSolid.EDIT, 15, Color.rgb(5,5 ,5));
+        ButtonFactory.addIcon(collectionBtn, FontAwesomeSolid.LAYER_GROUP, 15, Color.rgb(5, 5, 5));
+        ButtonFactory.addIcon(learnBtn, FontAwesomeSolid.GRADUATION_CAP, 15, Color.rgb(5, 5, 5));
+        ButtonFactory.addIcon(settingsBtn, FontAwesomeSolid.COG, 15, Color.rgb(5, 5, 5));
+    }
+
+    @Override
+    public void setupStyle() {
+        this.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/norbjdk/museeditor/styles/components.css")).toExternalForm());
+        this.getStyleClass().add("navigation-bar");
+    }
+
+    @Override
+    public void setupLayout() {
+        getChildren().addAll(
+                homeBtn,
+                createProjectBtn,
+                openProjectBtn,
+                createSpacer(SpaceFactory.Direction.HORIZONTAL),
+                currentProjectBtn,
+                createSpacer(SpaceFactory.Direction.HORIZONTAL),
+                collectionBtn,
+                learnBtn,
+                settingsBtn
+        );
+    }
+
+    @Override
+    public void setupEventListeners() {
+
+    }
+
+    @Override
+    public void setupEventHandlers() {
+
+    }
+}
