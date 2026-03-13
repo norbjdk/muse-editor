ef0d516810abcfc08d25e39cc688a6ccda3add8d
ndominkiewicz <norbertd1410@gmail.com>
2026-03-13 23:19:11 +0100

    feat: add empty application views
    - Those views awaits for their full implementation, just created for ViewManager it's view adding method() for testing purposes

---
+++ src/main/java/com/norbjdk/museeditor/ui/view/HomeView.java
@@ -0,0 +1,42 @@
+package com.norbjdk.museeditor.ui.view;
+
+import com.norbjdk.museeditor.ui.model.Presentable;
+import com.norbjdk.museeditor.ui.model.Viewable;
+import javafx.scene.control.ScrollPane;
+
+public class HomeView extends ScrollPane implements Presentable, Viewable {
+
+    public HomeView() {
+
+    }
+
+    @Override
+    public void initComponents() {
+
+    }
+
+    @Override
+    public void setupComponents() {
+
+    }
+
+    @Override
+    public void setupStyle() {
+
+    }
+
+    @Override
+    public void setupLayout() {
+
+    }
+
+    @Override
+    public void setupEventListeners() {
+
+    }
+
+    @Override
+    public void setupEventHandlers() {
+
+    }
+}
