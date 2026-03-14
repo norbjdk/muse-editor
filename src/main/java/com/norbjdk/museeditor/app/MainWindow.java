7e9d885988fdc615c45e6f1472d21bd2b0f097aa
ndominkiewicz <norbertd1410@gmail.com>
2026-03-14 15:54:24 +0100

    feat: add MenuBar as top Node alongside with NavigationBar

--- src/main/java/com/norbjdk/museeditor/app/MainWindow.java
+++ src/main/java/com/norbjdk/museeditor/app/MainWindow.java
@@ -1,8 +1,10 @@
 package com.norbjdk.museeditor.app;
 
+import com.norbjdk.museeditor.ui.component.MenuBar;
 import com.norbjdk.museeditor.ui.component.NavigationBar;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class MainWindow {
     private static final int WINDOW_WIDTH = 1280;
@@ -11,13 +13,15 @@ public class MainWindow {
     private final BorderPane root;
     private final Scene scene;
 
+    private final MenuBar menuBar;
     private final NavigationBar navigationBar;
 
     public MainWindow() {
+        menuBar = new MenuBar();
         navigationBar = new NavigationBar();
 
         root = new BorderPane();
-        root.setTop(navigationBar);
+        root.setTop(new VBox(0, menuBar, navigationBar));
 
         scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
     }
