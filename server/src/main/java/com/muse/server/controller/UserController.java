36d80f27ef6ae7a37a6909983ad17aa51e0014ee
ndominkiewicz <norbertd1410@gmail.com>
2026-03-24 16:49:33 +0100

    feat: add UserController with endpoints for fetching user info
    - GET /api/v1/users/me to get currently authenticated user
    - GET /api/v1/users/{username} to get user by username
    - integrate with UserService for data retrieval
    - configure CORS for frontend at http://localhost:5173

---
+++ server/src/main/java/com/muse/server/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.muse.server.controller;
+
+import com.muse.server.model.dto.UserResponse;
+import com.muse.server.model.repository.UserRepository;
+import com.muse.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getCurrentUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserResponse userResponse = userService.getUserByUsername(username);
+        return ResponseEntity.ok(userResponse);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserResponse> getUserByUsername(@PathVariable String username) {
+        UserResponse userResponse = userService.getUserByUsername(username);
+        return ResponseEntity.ok(userResponse);
+    }
+}
