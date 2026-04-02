a826a14e104d767e38d84434e8e3c6eac3858a0b
ndominkiewicz <norbertd1410@gmail.com>
2026-04-02 21:31:06 +0200

    feat: add login request and response dto's

---
+++ editor/src/main/java/com/muse/editor/core/model/dto/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.muse.editor.core.model.dto;
+
+public class LoginResponse {
+    private String token;
+    private String username;
+    private String email;
+
+    public LoginResponse() {}
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
