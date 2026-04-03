4eb34e24b009bdcb5cb0e006c113360516e9b728
ndominkiewicz <norbertd1410@gmail.com>
2026-04-04 01:03:41 +0200

    feat: add implementation for custom user details so auth response is going to return additionally email and id

--- server/src/main/java/com/muse/server/service/AuthService.java
+++ server/src/main/java/com/muse/server/service/AuthService.java
@@ -1,12 +1,12 @@
 package com.muse.server.service;
 
+import com.muse.server.model.detail.CustomUserDetails;
 import com.muse.server.model.dto.AuthRequest;
 import com.muse.server.model.dto.AuthResponse;
 import com.muse.server.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,10 +23,17 @@ public class AuthService {
 
     public AuthResponse auth(AuthRequest authRequest) {
         authManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-        final UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
+
+        CustomUserDetails userDetails = (CustomUserDetails) userService.loadUserByUsername(authRequest.getUsername());
+
         final String jwt = jwtUtil.generateToken(userDetails);
 
-        return new AuthResponse(jwt, userDetails.getUsername(), null);
+        return new AuthResponse(
+                jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail()
+        );
     }
 }
 
