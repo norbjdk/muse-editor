a3575423ab56ed69dc65a6fe5c87ff433b9f24b3
ndominkiewicz <norbertd1410@gmail.com>
2026-04-03 21:35:32 +0200

    refactor: replace mutable singleton with immutable User + Builder pattern

--- editor/src/main/java/com/muse/editor/core/user/User.java
+++ editor/src/main/java/com/muse/editor/core/user/User.java
@@ -3,13 +3,8 @@ package com.muse.editor.core.user;
 import com.muse.editor.core.project.Project;
 import javafx.beans.property.*;
 
-public class User {
-    private final static User instance = new User();
-
-    public static User getInstance() {
-        return instance;
-    }
-
+@Deprecated(forRemoval=true)
+class UserDeprecated {
     private final LongProperty idProperty = new SimpleLongProperty();
     private final StringProperty usernameProperty = new SimpleStringProperty();
     private final StringProperty emailProperty = new SimpleStringProperty();
@@ -17,7 +12,7 @@ public class User {
 
     private final BooleanProperty isLoggedProperty = new SimpleBooleanProperty(false);
 
-    private User() {}
+    private UserDeprecated() {}
 
     public long getId() {
         return idProperty.get();
@@ -53,5 +48,63 @@ public class User {
 
     public BooleanProperty getIsLoggedProperty() {
         return isLoggedProperty;
+    }
+}
+
+public class User {
+    private final long id;
+    private final String username;
+    private final String email;
+    private final String role;
+
+    private User(Builder builder) {
+        this.id = builder.id;
+        this.username = builder.username;
+        this.email = builder.email;
+        this.role = builder.role;
+    }
+
+    public User changeUsername(String username) {
+        return new Builder(this).setUsername(username).build();
+    }
+
+    public static class Builder {
+        private long id;
+        private String username;
+        private String email;
+        private String role;
+
+        public Builder(User existing) {
+            this.id = existing.id;
+            this.username = existing.username;
+            this.email = existing.email;
+            this.role = existing.role;
+        }
+
+        public Builder() {}
+
+        public Builder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder declareRole(String role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }
