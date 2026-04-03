bb3a5f9b8fefd76b91e93a1ee4ae986f0790c3a0
ndominkiewicz <norbertd1410@gmail.com>
2026-04-04 00:16:26 +0200

    feat: add id instance to store

--- editor/src/main/java/com/muse/editor/core/model/dto/LoginResponse.java
+++ editor/src/main/java/com/muse/editor/core/model/dto/LoginResponse.java
@@ -1,11 +1,20 @@
 package com.muse.editor.core.model.dto;
 
 public class LoginResponse {
+    private Long id;
     private String token;
     private String username;
     private String email;
 
     public LoginResponse() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getToken() { return token; }
     public void setToken(String token) { this.token = token; }
