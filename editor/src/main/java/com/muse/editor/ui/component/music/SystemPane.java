79eeb95625810a733cdeef9a775bd629c2f3af11
ndominkiewicz <norbertd1410@gmail.com>
2026-04-04 13:03:08 +0200

    feat: add system container with staffs

---
+++ editor/src/main/java/com/muse/editor/ui/component/music/SystemPane.java
@@ -0,0 +1,17 @@
+package com.muse.editor.ui.component.music;
+
+import com.muse.editor.ui.util.SheetMetrics;
+import javafx.scene.layout.VBox;
+
+public class SystemPane extends VBox {
+
+    private static final double SYSTEM_GAP = SheetMetrics.STAFF_SPACE * 2.5;
+
+    public SystemPane() {
+        setSpacing(SYSTEM_GAP);
+    }
+
+    public void addStaff(MeasurePane measurePane) {
+        getChildren().add(measurePane);
+    }
+}
