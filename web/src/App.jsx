122db6597721ae3f45e24f513336db2c77dfd2c1
ndominkiewicz <norbertd1410@gmail.com>
2026-04-11 17:49:52 +0200

    feat: add auth provider as it surrounds application routing

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,18 @@
 import './styles/global.css'
 import { Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext';
 import MainLayout from './layouts/MainLayout'
 import Home from './views/Home';
 
 function App() {
   return(
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path='/' element={<Home />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
