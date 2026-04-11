8cbce8aacc229c59ff8b2ce04f9f14ab1e06b5cd
ndominkiewicz <norbertd1410@gmail.com>
2026-04-12 00:02:48 +0200

    feat: add admin panel and upload views to routing

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -9,6 +9,8 @@ import Scores from './views/Scores';
 import Login from './views/Login';
 import Register from './views/Register';
 import Profile from './views/Profile';
+import AdminPanel from './views/AdminPanel';
+import Upload from './views/Upload';
 import Download from './views/Download';
 import Support from './views/Support';
 
@@ -23,6 +25,8 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/upload" element={<Upload />} />
           <Route path="/download" element={<Download />} />
           <Route path="/support" element={<Support />} />
           <Route path="*" element={<NotFound />} />
