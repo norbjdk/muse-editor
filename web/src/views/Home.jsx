657ce9a02f166e3abc7cc22c240349cc9665676b
ndominkiewicz <norbertd1410@gmail.com>
2026-04-11 19:58:25 +0200

    style: change feature card bg

--- web/src/views/Home.jsx
+++ web/src/views/Home.jsx
@@ -56,7 +56,7 @@ function Home() {
                 <div className={`min-w-9/12 max-w-full mx-auto`}>
                     <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8`}>
                         {features.map((feature, index) => (
-                            <div key={index} className={`flex items-center gap-4 bg-[#365603] border cursor-pointer border-[#CCC5B9]/10 p-3 rounded-lg hover:border-[#365603]/50 transition-all group hover:-translate-y-2`}>
+                            <div key={index} className={`flex items-center gap-4 bg-[#365603]/90 border cursor-pointer border-[#CCC5B9]/10 p-3 rounded-lg hover:border-[#365603]/50 transition-all group hover:-translate-y-2`}>
                                 <div className={`text-lg group-hover:scale-110 transition-transform duration-300`}>
                                     {feature.icon}
                                 </div>
