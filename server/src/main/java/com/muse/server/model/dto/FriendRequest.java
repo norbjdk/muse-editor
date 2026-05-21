ae25041f4023bad1133e200802834dfbcd9c82f7
norbjdk <norbertd1410@gmail.com>
2026-05-21 09:53:30 +0200

    feat: add dto for friendships and user social data

---
+++ server/src/main/java/com/muse/server/model/dto/FriendRequest.java
@@ -0,0 +1,15 @@
+package com.muse.server.model.dto;
+
+public class FriendRequest {
+    private Long friendId;
+
+    public FriendRequest() {}
+
+    public Long getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(Long friendId) {
+        this.friendId = friendId;
+    }
+}
---
+++ server/src/main/java/com/muse/server/model/dto/SocialResponse.java
@@ -0,0 +1,51 @@
+package com.muse.server.model.dto;
+
+public class SocialResponse {
+    private String youtubeId;
+    private String spotifyId;
+    private String instagramId;
+    private String tiktokId;
+    private String personalWebUrl;
+
+    public SocialResponse() {}
+
+    public String getYoutubeId() {
+        return youtubeId;
+    }
+
+    public void setYoutubeId(String youtubeId) {
+        this.youtubeId = youtubeId;
+    }
+
+    public String getSpotifyId() {
+        return spotifyId;
+    }
+
+    public void setSpotifyId(String spotifyId) {
+        this.spotifyId = spotifyId;
+    }
+
+    public String getInstagramId() {
+        return instagramId;
+    }
+
+    public void setInstagramId(String instagramId) {
+        this.instagramId = instagramId;
+    }
+
+    public String getTiktokId() {
+        return tiktokId;
+    }
+
+    public void setTiktokId(String tiktokId) {
+        this.tiktokId = tiktokId;
+    }
+
+    public String getPersonalWebUrl() {
+        return personalWebUrl;
+    }
+
+    public void setPersonalWebUrl(String personalWebUrl) {
+        this.personalWebUrl = personalWebUrl;
+    }
+}
