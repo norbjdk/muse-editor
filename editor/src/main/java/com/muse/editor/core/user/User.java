00e8c640176699729cdb7ba2381076afab5ed2e6
ndominkiewicz <norbertd1410@gmail.com>
2026-05-24 15:10:25 +0200

    refactor: remove deprecated user object

--- editor/src/main/java/com/muse/editor/core/user/User.java
+++ editor/src/main/java/com/muse/editor/core/user/User.java
@@ -5,54 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.muse.editor.core.project.Project;
 import javafx.beans.property.*;
 
-@Deprecated(forRemoval=true)
-class UserDeprecated {
-    private final LongProperty idProperty = new SimpleLongProperty();
-    private final StringProperty usernameProperty = new SimpleStringProperty();
-    private final StringProperty emailProperty = new SimpleStringProperty();
-    private final StringProperty roleProperty = new SimpleStringProperty();
-
-    private final BooleanProperty isLoggedProperty = new SimpleBooleanProperty(false);
-
-    private UserDeprecated() {}
-
-    public long getId() {
-        return idProperty.get();
-    }
-
-    public LongProperty getIdProperty() {
-        return idProperty;
-    }
-
-    public String getUsername() {
-        return usernameProperty.get();
-    }
-
-    public StringProperty getUsernameProperty() {
-        return usernameProperty;
-    }
-
-    public String getEmail() {
-        return emailProperty.get();
-    }
-
-    public StringProperty getEmailProperty() {
-        return emailProperty;
-    }
-
-    public String getRole() {
-        return roleProperty.get();
-    }
-
-    public StringProperty getRoleProperty() {
-        return roleProperty;
-    }
-
-    public BooleanProperty getIsLoggedProperty() {
-        return isLoggedProperty;
-    }
-}
-
 @JsonDeserialize(builder = User.Builder.class)
 public class User {
     private final long id;
