b5fdcda46fa803c1d577fa606350ec4b62eb8d38
ndominkiewicz <norbertd1410@gmail.com>
2026-05-24 03:25:00 +0200

    feat: add upload for:
    - avatar, mxml, project cover

--- server/src/main/java/com/muse/server/service/MinioService.java
+++ server/src/main/java/com/muse/server/service/MinioService.java
@@ -1,15 +1,15 @@
 package com.muse.server.service;
 
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
+import io.minio.errors.ErrorResponseException;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 @Service
 public class MinioService {
@@ -60,5 +60,107 @@ public class MinioService {
         } catch (Exception e) {
             throw new RuntimeException("Failed to create folder: " + path, e);
         }
+    }
+
+    public String uploadAvatar(Long userId, MultipartFile file) {
+        final String contentType = file.getContentType();
+        if (contentType == null || !contentType.startsWith("image/")) throw new IllegalArgumentException("File must be an image");
+
+        final String extension = extractExtension(contentType);
+        final String path      = userId + "/avatar/avatar" + extension;
+
+        addFile(path, file);
+
+        return buildUrl(path);
+    }
+
+    public String uploadProjectFile(Long userId, Long projectId, MultipartFile file) {
+        final List<String> allowed = List.of(
+                "application/xml",
+                "text/xml",
+                "application/octet-stream"
+        );
+
+        final String contentType = file.getContentType();
+        final String fileName    = file.getOriginalFilename();
+
+        if (contentType == null || !allowed.contains(contentType)) throw new IllegalArgumentException("File must be a xml type");
+        if (fileName == null || !(fileName.endsWith(".musicxml") || fileName.endsWith(".xml"))) throw new IllegalArgumentException("File must be a musicxml file");
+
+        final String extension = extractExtension(contentType);
+        final String path      = userId + "/projects/published/" + projectId + "/score"+ extension;
+
+        addFile(path, file);
+
+        return buildUrl(path);
+    }
+
+    public String uploadProjectCover(Long userId, Long projectId, MultipartFile file) {
+        final String contentType = file.getContentType();
+        if (contentType == null || !contentType.startsWith("image/")) throw new IllegalArgumentException("File must be an image");
+
+        final String extension = extractExtension(contentType);
+        final String path      = userId + "/projects/published/" + projectId + "/cover" + extension;
+
+        addFile(path, file);
+
+        return buildUrl(path);
+    }
+
+    public String extractProjectFileUrl(Long userId, Long projectId) {
+        final List<String> extensions = List.of(".musicxml", ".xml");
+
+        for (String extension : extensions) {
+            final String path = userId + "/projects/published/" + projectId + "/score" + extension;
+            if (fileExists(path)) return buildUrl(path);
+        }
+
+        throw new RuntimeException("Project file not found. ID:" + projectId);
+    }
+
+    private void addFile(String path,MultipartFile  file) {
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucket)
+                            .object(path)
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Upload failed: " + path, e);
+        }
+    }
+
+    private String extractExtension(String contentType) {
+        if (contentType == null) return ".bin";
+
+        return switch (contentType) {
+            case "image/jpeg" -> ".jpg";
+            case "image/png"  -> ".png";
+            case "image/webp" -> ".webp";
+            default           -> ".bin";
+        };
+    }
+
+    private boolean fileExists(String path) {
+        try {
+            minioClient.statObject(
+                    StatObjectArgs.builder()
+                            .bucket(bucket)
+                            .object(path)
+                            .build()
+            );
+            return true;
+        } catch (ErrorResponseException e) {
+            return false;
+        } catch (Exception e) {
+            throw new RuntimeException("Stat failed: " + path, e);
+        }
+    }
+
+    private String buildUrl(String path) {
+        return String.format("%s/%s/%s", endpoint,bucket, path);
     }
 }
