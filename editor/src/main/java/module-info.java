fbd9522b269f111faf210f324b4c0926e41739c0
ndominkiewicz <norbertd1410@gmail.com>
2026-06-10 01:51:21 +0200

    chore: add open for jackson databind

--- editor/src/main/java/module-info.java
+++ editor/src/main/java/module-info.java
@@ -19,6 +19,8 @@ module com.norbjdk.museeditor {
 
     opens com.muse.editor.develop.core.model.dto to com.fasterxml.jackson.databind;
     opens com.muse.editor.develop.core.user to com.fasterxml.jackson.databind;
+    opens com.muse.editor.redevelop.core.model.dto to com.fasterxml.jackson.databind;
+    opens com.muse.editor.redevelop.core.user to com.fasterxml.jackson.databind;
     exports com.muse.editor.redevelop.app.window;
     opens com.muse.editor.redevelop.app.window to javafx.fxml;
     exports com.muse.editor.redevelop.core.api;
