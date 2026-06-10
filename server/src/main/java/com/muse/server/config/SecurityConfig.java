c34561ede5ee0c1f133c35f6cd2a791bb342b9c7
ndominkiewicz <norbertd1410@gmail.com>
2026-06-10 19:32:45 +0200

    feat: allow match for collab http request

--- server/src/main/java/com/muse/server/config/SecurityConfig.java
+++ server/src/main/java/com/muse/server/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
