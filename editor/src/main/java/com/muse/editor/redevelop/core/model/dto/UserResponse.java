9c4c4ab3056ea4333101296c1cf72b24b70fcb59
ndominkiewicz <norbertd1410@gmail.com>
2026-06-10 02:03:46 +0200

    fix: add missing role field in response

--- editor/src/main/java/com/muse/editor/redevelop/core/model/dto/UserResponse.java
+++ editor/src/main/java/com/muse/editor/redevelop/core/model/dto/UserResponse.java
@@ -6,6 +6,7 @@ public class UserResponse implements ResponseDTO {
     private Long id;
     private String username;
     private String email;
+    private String role;
 
     public UserResponse() {}
 
@@ -22,4 +23,12 @@ public class UserResponse implements ResponseDTO {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
