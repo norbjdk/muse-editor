891859c9fa5f17e283368e6f5623c9a516ab5279
ndominkiewicz <norbertd1410@gmail.com>
2026-06-13 01:56:05 +0200

    feat: add save to file util method

--- editor/src/main/java/com/muse/editor/gui/util/SnapshotUtil.java
+++ editor/src/main/java/com/muse/editor/gui/util/SnapshotUtil.java
@@ -5,6 +5,12 @@ import javafx.scene.SnapshotParameters;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.VBox;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class SnapshotUtil {
     private static final SnapshotUtil instance = new SnapshotUtil();
 
@@ -26,5 +32,25 @@ public class SnapshotUtil {
 
     public WritableImage snapSheet() {
         return sheetPage.snapshot(new SnapshotParameters(), null);
+    }
+
+    public File saveToTempFile(WritableImage image) throws IOException {
+        File tempFile = Files.createTempFile("sheet_snapshot_", ".png").toFile();
+
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int argb = image.getPixelReader().getArgb(x, y);
+                bufferedImage.setRGB(x, y, argb);
+            }
+        }
+
+        ImageIO.write(bufferedImage, "png", tempFile);
+
+        return tempFile;
     }
 }
