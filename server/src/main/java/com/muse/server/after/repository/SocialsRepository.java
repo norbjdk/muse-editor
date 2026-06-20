497b1b8f38abb6193a7989d8859260cf5d3569cb
norbjdk <norbertd1410@gmail.com>
2026-06-20 16:12:21 +0200

    feat: add implementation to socials repo

--- server/src/main/java/com/muse/server/after/repository/SocialsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.muse.server.after.repository;
-
-import com.muse.server.before.model.entity.SocialEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocialsRepository extends JpaRepository<SocialEntity, Long> {
-}
---
+++ server/src/main/java/com/muse/server/after/repository/UserSocialsRepository.java
@@ -0,0 +1,23 @@
+package com.muse.server.after.repository;
+
+import com.muse.server.after.entity.UserSocialsEntity;
+import com.muse.server.before.model.entity.SocialEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserSocialsRepository extends JpaRepository<SocialEntity, Long> {
+    Optional<UserSocialsEntity> findByUserId(Long userId);
+
+    boolean existsByUserId(Long userId);
+
+    boolean existsByYoutubeId(String youtubeId);
+    boolean existsBySpotifyId(String spotifyId);
+    boolean existsByInstagramId(String instagramId);
+    boolean existsByTiktokId(String tiktokId);
+
+    Optional<UserSocialsEntity> findByYoutubeId(String youtubeId);
+    Optional<UserSocialsEntity> findBySpotifyId(String spotifyId);
+    Optional<UserSocialsEntity> findByInstagramId(String instagramId);
+    Optional<UserSocialsEntity> findByTiktokId(String tiktokId);
+}
