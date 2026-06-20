a591b3408b224b89948ecd9785a91e040c3f778d
norbjdk <norbertd1410@gmail.com>
2026-06-20 12:58:25 +0200

    refactor: add spring boot application init

---
+++ server/src/main/java/com/muse/server/after/MuseServerApp.java
@@ -0,0 +1,11 @@
+package com.muse.server.after;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseServerApp {
+    static void main(String[] args) {
+        SpringApplication.run(MuseServerApp.class, args);
+    }
+}
