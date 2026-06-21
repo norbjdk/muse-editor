5b2995898b797e9782d886df673e011ab586a2e0
norbjdk <norbertd1410@gmail.com>
2026-06-21 14:34:35 +0200

    feat: turn off init

--- server/src/main/java/com/muse/server/before/MuseServerApp.java
+++ server/src/main/java/com/muse/server/before/MuseServerApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MuseServerApp {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MuseServerApp.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(MuseServerApp.class, args);
+//    }
 
 }
